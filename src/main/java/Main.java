1dac1645d65ca969df89f46f0b3e18acdff2a67a
barbaralai <xil154@pitt.edu>
2016-02-03 15:33:26 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,42 +13,14 @@ import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
-/*edit 1.9*/
-/*import static javax.measure.unit.SI.KILOGRAM;
-import javax.measure.quantity.Mass;
-import org.jscience.physics.model.RelativisticModel;
-import org.jscience.physics.amount.Amount;*/
-
 public class Main {
 
   public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
-
-    get("/hello", (req, res) -> {
-      RelativisticModel.select();
-      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-      return "E=mc^2: 12 GeV = " + m.toString();
-    });
-/*
-    get("/hello", (req, res) -> {
-      RelativisticModel.select();
-
-      String energy = System.getenv().get("ENERGY");
-
-      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-      return "E=mc^2: " + energy + " = " + m.toString();
-    });
-*/
-    get("/hello", (req, res) -> "Hello World");
     
-    /*get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());*/
+    get("/hello", (req, res) -> "Hello World");
 
     get("/db", (req, res) -> {
       Connection connection = null;
