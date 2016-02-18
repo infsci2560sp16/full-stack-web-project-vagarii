ccb409f069fa6e91186d7580f282bac099e0440e
vagarii <xil154@pitt.edu>
2016-02-18 18:30:43 -0500

    edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,7 +26,7 @@ public class Main {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "home.html");
+            return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
